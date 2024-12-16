6992c251e326404e5ab0f6a03fdcd65eff386dc6
jasverma-hub <jazverma18@gmail.com>
2024-12-16 17:01:48 +0530

    updated "page"

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <SpeedInsights />
+        <Analytics />
         <FloatingNav navItems={navItems} />
         <Hero />
         <Approach />
