5f0b28be96fabe01583df969e6c844a5b5008efa
jasverma-hub <jazverma18@gmail.com>
2024-12-16 16:41:54 +0530

    added "SpeedInsights"

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <SpeedInsights />
         <FloatingNav navItems={navItems} />
         <Hero />
         <Approach />
